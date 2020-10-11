414e6a347244786fedd2b17845ccbb0eda9355ca
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2020-10-11 14:27:57 -0400

    Add ratelimit and blacklisting codes, ips, and url hosts

--- src/routes/api/v1/shorten/index.mjs
+++ src/routes/api/v1/shorten/index.mjs
@@ -8,6 +8,12 @@ export default async (fastify, options, next) => {
     fastify.route({
         method: "POST",
         url: '/',
+        config: {
+            rateLimit: {
+                max: 15,
+                timeWindow: '5 minutes'
+            }
+        },
         schema: {
             body: {
                 type: "object",
@@ -27,12 +33,52 @@ export default async (fastify, options, next) => {
             }
         },
         handler: async (req, res) => {
+            var ip = req.headers['cf-connecting-ip'] || req.raw.ip
+            var blacklisted_codes = await fastify.mongo.db.collection('misc').findOne({
+                stat: 'blacklisted-codes'
+            })
+            var banned_hosts = await fastify.mongo.db.collection('misc').findOne({
+                stat: 'banned-hosts'
+            })
+            var banned_ips = await fastify.mongo.db.collection('misc').findOne({
+                stat: 'banned-ips'
+            })
+            if (!blacklisted_codes) {
+                await fastify.mongo.db.collection('misc').insertOne({
+                    stat: 'blacklisted-codes',
+                    codes: []
+                })
+                blacklisted_codes = {
+                    codes: []
+                }
+            }
+            if (!banned_hosts) {
+                await fastify.mongo.db.collection('misc').insertOne({
+                    stat: 'banned-hosts',
+                    hosts: []
+                })
+                banned_hosts = {
+                    hosts: []
+                }
+            }
+            if (!banned_ips) {
+                await fastify.mongo.db.collection('misc').insertOne({
+                    stat: 'banned-ips',
+                    ips: []
+                })
+                banned_ips = {
+                    ips: []
+                }
+            }
+            if (banned_ips.ips.indexOf(ip) !== -1) return res.code(400).send({
+                message: 'You have been banned from using this service.'
+            })
             var domain = req.body.domain ? req.body.domain.toLowerCase() : req.hostname
             var url = new URL(req.body.url)
             if (['https:', 'http:'].indexOf(url.protocol) == -1) return res.code(400).send({
                 message: 'URL Invalid, Bad Protocol'
             })
-            if ([...[...fastify.plugins].map(e => e[0]), ...fastify.config.domains].indexOf(url.hostname) !== -1) return res.code(400).send({
+            if ([...[...fastify.plugins].map(e => e[0]), ...fastify.config.domains, ...banned_hosts.hosts].indexOf(url.hostname) !== -1) return res.code(400).send({
                 message: 'URL Invalid, Bad Domain'
             })
             var code = ''
@@ -40,6 +86,9 @@ export default async (fastify, options, next) => {
                 var plugin = fastify.plugins.get(domain)
                 if (!plugin.options.custom_codes && req.body.code) return res.code(400).send({
                     message: "This domain doesn't support custom codes!"
+                })
+                if (blacklisted_codes.codes.indexOf(req.body.code) !== -1) return res.code(400).send({
+                    message: 'Code Invalid (Blacklisted)'
                 })
                 try {
                     code = await (new Promise((res, rej) => {
@@ -64,6 +113,9 @@ export default async (fastify, options, next) => {
                 if (req.body.code) {
                     if (!/^[\w]{0,20}$/.test(req.body.code)) return res.code(400).send({
                         message: 'Short code must only conain alphanumeric characters. (a-z 0-9)'
+                    })
+                    if (blacklisted_codes.codes.indexOf(req.body.code.toLowerCase()) !== -1) return res.code(400).send({
+                        message: 'Code Invalid (Blacklisted)'
                     })
                     if (await fastify.mongo.db.collection('urls').findOne({
                             code: req.body.code,
